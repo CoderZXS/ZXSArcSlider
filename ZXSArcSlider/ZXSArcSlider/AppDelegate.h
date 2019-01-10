246eab02dac3c0ea56e81e60f980f9454bf11091
bi xu <bixu@bideiMac-2.local>
2019-01-10 11:41:03 +0800

    1

---
+++ ZXSArcSlider/ZXSArcSlider/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZXSArcSlider
+//
+//  Created by bi xu on 2019/1/10.
+//  Copyright © 2019 Cammus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZXSArcSlider/ZXSArcSlider/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZXSArcSlider
+//
+//  Created by bi xu on 2019/1/10.
+//  Copyright © 2019 Cammus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
